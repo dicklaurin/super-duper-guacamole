df822344107f74e1df2a3394ca2abdc074f3c237
dicklaurin <laurin.dick@rwth-aachen.de>
2016-11-26 16:27:35 +0100

    Add files via upload

---
+++ Material.java
@@ -0,0 +1,13 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Laurin
+ */
+public enum Material {
+    Wood, Stone, Metal, Plastic, Concrete, ReinforcedConcrete;
+}
