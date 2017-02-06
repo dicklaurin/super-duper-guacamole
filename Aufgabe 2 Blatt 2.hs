2448d2ad88dcfed8465aefe8ad2922900c89b5d7
dicklaurin <laurin.dick@rwth-aachen.de>
2017-02-06 06:23:41 +0100

    Theoretisch funktionert es :/
    Send Help

--- Aufgabe 2 Blatt 2.hs
+++ Aufgabe 2 Blatt 2.hs
@@ -5,41 +5,44 @@ t1 = Knot 3 42[Knot 3 15[Leaf 3, Leaf 11, Leaf 12] , Knot 19 42[Leaf 42, Leaf 23
 
 
 
-verzweigungKnoten:: MultTree  -> Int
-verzweigungKnoten x y [] =0
+verzweigungKnoten:: MultTree Int -> Int
+verzweigungKnoten x y [] = 0
 verzweigungKnoten x y (a:b:[])=2
 verzweigungKnoten x y (a:b:c) = 1+ verzweigungKnoten x y (b:c) 
 
 
 
-verzweigungsgrad :: MultTree -> Int
+verzweigungsgrad :: MultTree Int -> Int
 verzweigungsgrad x y (a:b) = max(verzweigungKnoten this , verzweigungKnoten a, verzweigungsgrad b)
 verzweigungsgrad x y [] =0
 verzweigungsgrad x =0
 verzweigungsgrad (a:b) = Prelude.max (verzweigungsgrad a, verzweigungsgrad b)
 
 
-datenListe :: MultTree -> [Int]
+datenListe :: MultTree Int -> [Int]
 datenListe x y (a:[]) = datenListe a
 datenListe x y ([]) = Nil
 datenListe x y (a:b)= datenListe a ++ datenListe b
 datenListe x = [x]
 
-datenIntervalle :: MultTree -> MultTree
-datenIntervalle x y (a:b) = if (datenLesen (a:b) ) then ((if (x>ListenMin(datenIntervalle a:b))  then x = datenIntervalle a),  (if y<ListenMax(datenIntervalle a:b) then (let y=a )) ) else (datenIntervalle a, datenIntervalle b)
+datenIntervalle :: MultTree Int -> MultTree Int
+datenIntervalle x y (a:b) = if (datenLesen (a:b) ) then if (x>listenMin(datenIntervalle a:b) && y<listenMax(datenIntervalle a:b) ) then listenMin(datenIntervalle a:b) listenMax(datenIntervalle a:b) (a:b) else if y<listenMax(datenIntervalle a:b) then x listenMax(datenIntervalle a:b) (a:b)   else if x>listenMin(datenIntervalle a:b) then listenMin(datenIntervalle a:b) y (a:b) else () else x y (datenIntervalle a : datenIntervalle b)
+-- ganz kurz gefasst ist diese geschachtelte if klammer so aufgebaut: wenn die nachfolger vom jetzigen Knoten blätter sind wird nach den höchsten und niedrigsten Werten geschaut und dann die rekursion in den jeweiligen Fällen fortgeführt. 
+-- z.B. wenn eine zahl größer als y ist, wird  x (die zahl die größer als y und das maximum der Liste ist, die von datenIntervalle ausgegeben wird ist) (a:b) ausgegeben. wenn keine blätter getroffen werden, wird datenIntervalle der Nachfolger aufgerufen.
 datenIntervalle x = x
 datenIntervalle (a:b) = datenIntervalle a ++ datenIntervalle b
 datenIntervalle (a:[]) = datenIntervalle a
 
-datenLesen [MultTree] -> Bool
-datenLesen ((x y [z]): b) = false
-datenLesen x = true
-datenlesen (x: b) =  datenLesen b
+datenLesen:: [MultTree Int]-> Bool
+datenLesen [x]= datenLesen x
+datenLesen x y [z] = false
+datenLesen (x:[]) = true
+datenLesen (x: b) =  datenLesen b
 
-ListenMin :: [Int] -> Int
-ListenMin (a:[]) = a
-ListenMin (a:b:c) = if(a>b) then ListenMin (b:c) else ListenMin (a:c)
+listenMin :: [Int] -> Int
+listenMin (a:[]) = a
+listenMin (a:b:c) = if(a>b) then listenMin (b:c) else listenMin (a:c)
 
-ListenMax :: [Int] -> Int
-ListenMax (a:[]) = a
-ListenMax (a:b:c) = if(a<b) then ListenMax (b:c) else ListenMax (a:c)
+listenMax :: [Int] -> Int
+listenMax (a:[]) = a
+listenMax (a:b:c) = if(a<b) then listenMax (b:c) else listenMax (a:c)
